6f4961ea55934684650f5a9eafef54fd0fb8fa8b
Danielle Byrne <DanielleByrne421@gmail.com>
2021-11-04 18:35:13 -0700

    practicing before the next class

---
+++ script.js
@@ -0,0 +1,47 @@
+//Boolean Data: true of false , go along with if/then statements 
+// == equal
+// === strictly equal (type matters)
+// > grater
+// < less
+// && and
+// || or
+// >= greater/equal
+// <= less/equal
+
+// all evaluate to  true or false
+
+// If Statements
+
+// const costOfMilk = 4.99
+
+// if( costOfMilk < 2){
+//     console.log("buy two gallons")
+// } else if(costOfMilk < 3) {
+//     console.log("but only one gallon")
+// } else{
+//     console.log("not buying milk this week")
+// }
+
+//Switch Statement 
+
+// const grade = "F"
+
+// switch (grade){
+//     case "A":
+//         console.log("range 90-100")
+//         break;
+//     case "B":
+//         console.log("range 80-89")
+//         break;
+//     case "C":
+//         console.log("range 70-79")
+//         break;
+//     case "D":
+//         console.log("range 60-69")
+//         break;
+//     default:
+//         console.log("range 0-69")
+// }
+
+//flattening: taking nested if statements and combining them using &&, but 
+//not if the second statement is going to determine a different path
